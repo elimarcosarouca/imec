07d05de82132417b3a8545da5478c74621da0db3
claudemirramosferreira <claudemirramosferreira@gmail.com>
2014-11-21 13:06:13 +0000

    aa

--- ECM/FONTES/Fontes/ecmweb/src/main/java/br/com/ss/model/servico/EmpresaServico.java
+++
@@ -1,7 +0,0 @@
-package br.com.ss.model.servico;
-
-import br.com.ss.model.entidade.Empresa;
-
-public interface EmpresaServico extends Servico<Empresa, Long> {
-
-}
--- ECM/FONTES/Fontes/ecmweb/src/main/java/br/com/ss/model/servico/UsuarioPerfilServico.java
+++
@@ -1,7 +0,0 @@
-package br.com.ss.model.servico;
-
-import br.com.ss.model.entidade.UsuarioPerfil;
-
-public interface UsuarioPerfilServico extends Servico<UsuarioPerfil, Long> {
-
-}
