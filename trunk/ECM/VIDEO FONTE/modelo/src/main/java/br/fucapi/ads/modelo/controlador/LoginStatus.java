a55aeee876e0b395dc29c1b024cf773634f6e509
claudemirramosferreira@gmail.com <claudemirramosferreira@gmail.com@10514f96-a0f7-054e-f3e3-1ddd370b22be>
2015-01-18 14:35:15 +0000

    ajuste no start do processo

--- trunk/ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/controlador/LoginStatus.java
+++ trunk/ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/controlador/LoginStatus.java
@@ -1,10 +1,14 @@
 package br.fucapi.ads.modelo.controlador;
 
+import java.io.Serializable;
+
 import org.springframework.stereotype.Component;
 
 @Component
-public class LoginStatus {
+public class LoginStatus implements Serializable{
 
+	private static final long serialVersionUID = 2437881738814327976L;
+	
 	private boolean showMsgErro;
 	
 	public boolean isShowMsgErro() {
