99b8e2694c1deea815130497d7822ea4ba6f97f9
claudemirramosferreira <claudemirramosferreira@10514f96-a0f7-054e-f3e3-1ddd370b22be>
2015-02-06 11:08:14 +0000

    dddd

--- trunk/ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/converter/PostoCopiaConverter.java
+++ trunk/ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/converter/PostoCopiaConverter.java
@@ -1,64 +1,28 @@
 package br.fucapi.ads.modelo.converter;
 
-import java.util.List;
-
-import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
-import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
-import br.fucapi.ads.modelo.controlador.PostoCopiaControlador;
-import br.fucapi.ads.modelo.dominio.Setor;
+import br.fucapi.ads.modelo.dominio.PostoCopia;
 
 @FacesConverter(value = "postoCopiaConverter")
 public class PostoCopiaConverter implements Converter {
 
-	public Object getAsObject(FacesContext facesContext, UIComponent uicomp,
-			String submittedValue) {
-		if (submittedValue.trim().equals("")) {
-			return null;
-		}
-		Long setorId = Long.parseLong(submittedValue);
-		Setor setor = lookupSetor(setorId);
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {
 
-		if (setor == null) {
-			throw new ConverterException(new FacesMessage(
-					FacesMessage.SEVERITY_ERROR, "Conversion Error",
-					"Id do Responsavel não válido"));
-		}
-		return setor;
+		return PostoCopia.fromJsonToPostoCopia(value);
 	}
 
-	private Setor lookupSetor(Long setorId) {
-		for (Setor unid : getSetorList()) {
-			if (unid.getId().equals(setorId)) {
-				return unid;
-			}
-		}
-		return null;
-	}
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
 
-	/**
-	 * Recupera a lista que foi carregada na lista retornada para o autocomplete
-	 * do managed bean.
-	 * 
-	 * @return
-	 */
-	private List<Setor> getSetorList() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		PostoCopiaControlador controlador = (PostoCopiaControlador) context
-				.getELContext()
-				.getELResolver()
-				.getValue(context.getELContext(), null, "postoCopiaControlador");
-		List<Setor> setoresList = (List<Setor>) controlador.getSetores();
-		return setoresList;
-	}
+		return ((PostoCopia) value).toJson().replace("\"", "\'");
 
-	public String getAsString(FacesContext facesContext, UIComponent uicomp,
-			Object entity) {
-		return entity == null ? "-1" : ((Setor) entity).getId().toString();
 	}
 
 }
--- trunk/ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/converter/UnidadeConverter.java
+++ trunk/ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/converter/UnidadeConverter.java
@@ -1,58 +1,36 @@
 package br.fucapi.ads.modelo.converter;
 
-import java.util.List;
-
-import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
-import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
-import br.fucapi.ads.modelo.controlador.SetorControlador;
 import br.fucapi.ads.modelo.dominio.Unidade;
 
 @FacesConverter(value = "unidadeConverter")
 public class UnidadeConverter implements Converter {
-	
-	public Object getAsObject(FacesContext facesContext, UIComponent uicomp,
-			String submittedValue) {
-		if (submittedValue.trim().equals("")) {
-			return null;
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {
+
+		if ("".equals(value) ) {
+			return "";
 		}
-		Long unidadeId = Long.parseLong(submittedValue);
-		Unidade unidade = lookupUnidade(unidadeId);
 		
-		if (unidade == null) {
-			throw new ConverterException(new FacesMessage( FacesMessage.SEVERITY_ERROR, "Conversion Error",
-					"Id do Responsavel não válido"));
+		return Unidade.fromJsonToUnidade(value);
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
+
+		if ("".equals(value) ) {
+			return "";
 		}
-		return unidade;
-	}
+		
+		return ((Unidade) value).toJson().replace("\"", "\'");
 
-	private Unidade lookupUnidade(Long responsavelId) {
-		for (Unidade unid : getUnidadeList() ) {
-			if(unid.getId().equals(responsavelId)) {
-				return unid;
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Recupera a lista que foi carregada na lista retornada para o autocomplete do managed bean.
-	 * @return
-	 */
-	private List<Unidade> getUnidadeList() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		SetorControlador controlador = (SetorControlador) context.getELContext()
-				.getELResolver().getValue(context.getELContext(), null, "setorControlador");
-		List<Unidade> unidadeList = (List<Unidade>) controlador.getUnidades();
-		return unidadeList;
-	}
-
-	public String getAsString(FacesContext facesContext, UIComponent uicomp, Object entity) {
-		return entity == null ? "-1" : ((Unidade) entity).getId().toString();
 	}
 
 }
