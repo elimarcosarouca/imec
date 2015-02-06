997ba26a376bb38818357d9edb70ebbc95b1e71d
claudemirramosferreira <claudemirramosferreira@10514f96-a0f7-054e-f3e3-1ddd370b22be>
2015-02-06 11:10:44 +0000

    ss

--- trunk/ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/excecaoHandler/ExcecaoHandler.java
+++ trunk/ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/excecaoHandler/ExcecaoHandler.java
@@ -2,7 +2,6 @@ package br.fucapi.ads.modelo.excecaoHandler;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.logging.Logger;
 
@@ -13,19 +12,12 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ExceptionQueuedEvent;
 import javax.faces.event.ExceptionQueuedEventContext;
 
-import org.primefaces.context.RequestContext;
-
-import br.fucapi.ads.mail.MailService;
-import br.fucapi.ads.mail.MailServiceImpl;
-import br.fucapi.ads.modelo.controlador.ErroControladorBean;
-import br.fucapi.ads.modelo.controlador.PaginaCentralControladorBean;
-
 public class ExcecaoHandler extends ExceptionHandlerWrapper {
 
 	private ExceptionHandler wrapped;
 	private static final Logger log = Logger.getLogger(ExcecaoHandler.class
 			.getCanonicalName());
-	
+
 	public ExcecaoHandler(ExceptionHandler wrapped) {
 		this.wrapped = wrapped;
 	}
@@ -65,31 +57,37 @@ public class ExcecaoHandler extends ExceptionHandlerWrapper {
 				log.severe("[Exception ID: #" + tempo + "]\n\t"
 						+ stackTrace.toString());
 
-//				PaginaCentralControladorBean paginaCentralControladorBean = (PaginaCentralControladorBean) fc
-//						.getApplication().evaluateExpressionGet(fc,
-//								"#{paginaCentralControladorBean}",
-//								PaginaCentralControladorBean.class);
+				// PaginaCentralControladorBean paginaCentralControladorBean =
+				// (PaginaCentralControladorBean) fc
+				// .getApplication().evaluateExpressionGet(fc,
+				// "#{paginaCentralControladorBean}",
+				// PaginaCentralControladorBean.class);
 
-//				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+				// Authentication authentication =
+				// SecurityContextHolder.getContext().getAuthentication();
 
-//				ErroControladorBean erroControladorBean = (ErroControladorBean) fc
-//						.getApplication().evaluateExpressionGet(fc,
-//								"#{erroControladorBean}",
-//								ErroControladorBean.class);
-				
-//				MailService mailService = (MailService) fc.getApplication().evaluateExpressionGet(fc, "#{mailService}", MailService.class);
-//
-//				erroControladorBean.setData(new Date(tempo));
-//				erroControladorBean.setPilhaExcecao(stackTrace.toString());
-////				erroControladorBean.setUsuario(authentication.getName());
-//				erroControladorBean.setEmail(((MailServiceImpl) mailService).getSender());
-//				erroControladorBean.setClassName(throwable.getCause()
-//						.getClass().getCanonicalName());
-//				erroControladorBean.setIdException(tempo);
-//
-//				paginaCentralControladorBean
-//						.setPaginaCentral("paginas/error.xhtml");
-//				RequestContext.getCurrentInstance().update("paginaCentral");
+				// ErroControladorBean erroControladorBean =
+				// (ErroControladorBean) fc
+				// .getApplication().evaluateExpressionGet(fc,
+				// "#{erroControladorBean}",
+				// ErroControladorBean.class);
+
+				// MailService mailService = (MailService)
+				// fc.getApplication().evaluateExpressionGet(fc,
+				// "#{mailService}", MailService.class);
+				//
+				// erroControladorBean.setData(new Date(tempo));
+				// erroControladorBean.setPilhaExcecao(stackTrace.toString());
+				// // erroControladorBean.setUsuario(authentication.getName());
+				// erroControladorBean.setEmail(((MailServiceImpl)
+				// mailService).getSender());
+				// erroControladorBean.setClassName(throwable.getCause()
+				// .getClass().getCanonicalName());
+				// erroControladorBean.setIdException(tempo);
+				//
+				// paginaCentralControladorBean
+				// .setPaginaCentral("paginas/error.xhtml");
+				// RequestContext.getCurrentInstance().update("paginaCentral");
 
 			} finally {
 				// Remove-lo da fila
