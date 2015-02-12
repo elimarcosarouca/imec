8cb53048648c7a007887a6dc1c54504a0161f43e
claudemirramosferreira <claudemirramosferreira@10514f96-a0f7-054e-f3e3-1ddd370b22be>
2015-02-12 10:55:06 +0000

    alterei o nome do converter

--- ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/converter/CategoriaConverter.java
+++ ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/converter/CategoriaConverter.java
@@ -5,31 +5,31 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
-import br.fucapi.ads.modelo.dominio.TipoDocumento;
+import br.fucapi.ads.modelo.dominio.Categoria;
 
-@FacesConverter(value = "tipoDocumentoConverter")
-public class TipoDocumentoConverter implements Converter {
+@FacesConverter(value = "categoriaConverter")
+public class CategoriaConverter implements Converter {
 
 	@Override
 	public Object getAsObject(FacesContext context, UIComponent component,
 			String value) {
 
-		if ("".equals(value) ) {
+		if ("".equals(value)) {
 			return "";
 		}
-		
-		return TipoDocumento.fromJsonToObject(value);
+
+		return Categoria.fromJsonToObject(value);
 	}
 
 	@Override
 	public String getAsString(FacesContext context, UIComponent component,
 			Object value) {
 
-		if ("".equals(value) ) {
+		if ("".equals(value)) {
 			return "";
 		}
-		
-		return ((TipoDocumento) value).toJson().replace("\"", "\'");
+
+		return ((Categoria) value).toJson().replace("\"", "\'");
 	}
 
 }
