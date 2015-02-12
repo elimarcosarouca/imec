9ad2498952b670f48d251df10f1147c4f24d4cba
claudemirramosferreira <claudemirramosferreira@10514f96-a0f7-054e-f3e3-1ddd370b22be>
2015-02-12 10:58:08 +0000

    alterie o nome dos servicos

---
+++ trunk/ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/servico/ColaboradorServico.java
@@ -0,0 +1,7 @@
+package br.fucapi.ads.modelo.servico;
+
+import br.fucapi.ads.modelo.dominio.Colaborador;
+
+public interface ColaboradorServico extends Servico<Colaborador, Long> {
+
+}
--- trunk/ECM/VIDEO FONTE/modelo/src/main/java/br/fucapi/ads/modelo/servico/FuncionarioServico.java
+++
@@ -1,7 +0,0 @@
-package br.fucapi.ads.modelo.servico;
-
-import br.fucapi.ads.modelo.dominio.Funcionario;
-
-public interface FuncionarioServico extends Servico<Funcionario, Long> {
-
-}
